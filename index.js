49b2d9f4a872b6f4b096009fad9baf38853d8791
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-07-14 19:57:23 +0200

    webhook receiver

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
-const port = 4000;
+const port = 8455;
+
+app.use('/', (req, res, next) => {
+  console.log('VVVVVVVVVVVVVVVVVVVVVVVVVVV');
+  console.log(req.body);
+  console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+  res.send();
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
