01a5d6b9b931af1068645d96bb95aa49c54d2d4d
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-07-14 20:06:42 +0200

    add bodyparser

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const port = 8455;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', (req, res, next) => {
