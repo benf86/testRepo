abb119f6ff9eaa3b3231bfd188926452438a7e33
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-07-15 02:32:41 +0200

    +1

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => req.integratorData = {} && next());
 app.use('/', ghAPI.handlePushNotification);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
