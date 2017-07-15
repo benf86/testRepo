22d9dee786fb58c359c1ae0575476c0c6e03f37f
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-07-15 02:19:05 +0200

    +1

--- github.js
+++ github.js
@@ -5,14 +5,13 @@ const githubRawPrefix = 'https://raw.githubusercontent.com';
 const getBlog = repository =>
 request.get(`${githubRawPrefix}/${repository.full_name}/master/blog.md`);
 
-const logBlog = (pusher, blog) => console.log(blog);
+const logBlog = (pusher, blog) => console.log(pusher, blog) || ({ pusher, blog });
 
 /* ************************************************************** */
 
 const parsePush = ({ repository, pusher }) =>
   getBlog(repository)
-  .tap(logBlog.bind(null, pusher))
-  .return({ repository, pusher });
+  .then(logBlog.bind(null, pusher));
 
 module.exports = {
   handlePushNotification: (req, res, next) => parsePush(JSON.parse(req.body.payload)).then(res.send),
