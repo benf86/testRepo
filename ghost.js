3da97da69c4d37bcb8113c4711e3dabf652c09d8
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-07-15 03:13:57 +0200

    +1

--- ghost.js
+++ ghost.js
@@ -2,21 +2,21 @@ const config = require('./config');
 const request = require('request-promise');
 
 
-const login = () => request.post(
-  `${config.ghost.api}/authentication/token`,
-  {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: {
-      grant_type: 'password',
-      username: config.ghost.username,
-      password: config.ghost.password,
-      client_id: config.ghost.clientId,
-      client_secret: config.ghost.clientSecret,
-    },
-  });
+const login = () => request({
+  method: 'POST',
+  uri: `${config.ghost.api}/authentication/token`,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: {
+    grant_type: 'password',
+    username: config.ghost.username,
+    password: config.ghost.password,
+    client_id: config.ghost.clientId,
+    client_secret: config.ghost.clientSecret,
+  },
+});
 
 
 const handleGhUpdate = (req, res, next) => {
